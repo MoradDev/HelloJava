cd1c10e6ddd943357fcf06a1ebb8c38826bc3bb7
MoradDev <72839584+MoradDev@users.noreply.github.com>
2023-11-15 09:38:26 +0100

    Update Hello.java
    premier job

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@
 
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Pipeline graphique !!!! "); 
+        System.out.println("Hello premier job git/jenkins "); 
     }
 }
